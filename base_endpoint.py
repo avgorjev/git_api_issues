ebfb06c9a10fedf01a75a040b830898f3be407c0
Andrei Gorev <93729791+avgorjev@users.noreply.github.com>
2024-07-09 13:54:04 +0400

    Update base_endpoint.py

--- base_endpoint.py
+++ base_endpoint.py
@@ -17,7 +17,7 @@ payload = {
 }
 headers = {
       'Accept': 'application/vnd.github+json',
-      'Authorization': f'Bearer {token}',
+      'Authorization': f'Bearer ${{ secrets.GITHUB_TOKEN }}',
       'X-GitHub-Api-Version': '2022-11-28',
       'Content-Type': 'application/json',
       'Cookie': '_octo=GH1.1.809836981.1720270408; logged_in=no'
